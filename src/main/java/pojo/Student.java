67be95a155f87f5803923034f828dec22bccb314
Peter <Peter@Peter-PC>
2018-01-08 17:20:27 +0800

    Student added

---
+++ src/main/java/pojo/Student.java
@@ -0,0 +1,26 @@
+package pojo;
+
+public class Student {
+
+	private Integer id;
+	private String name;
+	private String score;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getScore() {
+		return score;
+	}
+	public void setScore(String score) {
+		this.score = score;
+	}
+}
